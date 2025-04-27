970bcd3786ac6ad506d2f5ca019ae7d9d21c274e
ael_2 <240337@astanait.edu.kz>
2025-04-27 23:18:51 +0500

    //3

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-       MyHashTable<MyTestingClass, Student> table = new MyHashTable<>(66);
-       Random random = new Random();
+        MyHashTable<MyTestingClass, Student> table = new MyHashTable<>(66);
+        Random random = new Random();
 
         for (int i = 0; i < 10_000; i++) {
             int id = random.nextInt(100_000);
@@ -19,6 +19,29 @@ public class Main {
         int[] bucketSizes = table.getBucketSizes();
         for (int i = 0; i < bucketSizes.length; i++) {
             System.out.println("Bucket " + i + " contains " + bucketSizes[i] + " elements.");
+        }
+    }
+    private static void testBST() {
+        BST<Integer, String> tree = new BST<>();
+        tree.put(7, "Seven");
+        tree.put(8, "Eight");
+        tree.put(2, "Two");
+        tree.put(3, "Three");
+        tree.put(1, "One");
+        tree.put(3, "Three - Updated");
+
+        System.out.println("BST size before deletion: " + tree.size());
+
+        for (var elem : tree) {
+            System.out.println("Key is " + elem.key() + " and value is " + elem.value());
+        }
+
+        System.out.println("Value for key 3: " + tree.get(3));
+        tree.delete(2);
+        System.out.println("\n" + "BST size after deleting key 2: " + tree.size());
+        System.out.println("In-order traversal after deletion:");
+        for (var elem : tree) {
+            System.out.println("Key is " + elem.key() + " and value is " + elem.value());
         }
     }
 }
